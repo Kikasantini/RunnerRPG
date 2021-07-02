b0ff9e8e6de89e0c9a790de7905402f84e395633
Kikasantini <kikasantini@gmail.com>
2021-07-02 16:43:00 -0300

    New skill panel and color adjustments (CharMenu)

---
+++ Assets/Scripts/SkillPanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillPanel : MonoBehaviour
+{
+    public Text name;
+    public Text type;
+    public Text description;
+    public SkillSO[] skill;
+    public GameObject panel;
+
+    public void ShowSkillInfo(int index)
+    {
+        
+        name.text = "Name: " + skill[index].skillName;
+        
+        if (skill[index].isMagic)
+            type.text = "Type: Magical";
+        else
+            type.text = "Type: Physical";
+
+        description.text = skill[index].description;
+        panel.SetActive(true);
+    }
+
+    public void OnClick(int index)
+    {
+        ShowSkillInfo(index);
+    }
+}
